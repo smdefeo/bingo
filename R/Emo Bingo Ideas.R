6215f8a264fbb4edd949a6197f414611267489dd
Shelby Defeo <smdefeo@gmail.com>
2022-01-31 17:06:15 -0800

    Adding emo ideas to bingo

---
+++ R/Emo Bingo Ideas.R
@@ -0,0 +1,34 @@
+# install.packages("devtools")
+devtools::install_github("jennybc/bingo")
+library(bingo)
+
+#Theme: Emo bands you thought you would grow out of but never did
+#Categories by Shelby Defeo
+c('Bright Eyes',
+  'Mom Jeans',
+  'Blink 182',
+  'The Front Bottoms',
+  'Oso Oso',
+  'Front Porch Step',
+  'Modern Baseball',
+  'Walter Mitty and His Makeshift Orchestra',
+  'Michael Cera Palin',
+  'Jank',
+  'Scratcher',
+  'American Football',
+  'Remo Drive',
+  'Pool Kids',
+  'Free Throw',
+  'Sorority Noise',
+  'Marietta',
+  'Moose Blood',
+  'Hot Mulligan',
+  'Charmer',
+  'AJJ',
+  'Crywank',
+  'Paramore',
+  'Tigers Jaw')
+
+
+
+
